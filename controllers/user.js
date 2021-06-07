f93162fd8e80395e8830a52bf7e17121be2680d9
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2021-06-07 09:54:38 -0300

    feat: valid email

--- controllers/user.js
+++ controllers/user.js
@@ -1,17 +1,30 @@
 const { response } = require('express');
 const Usuario = require('../models/user');
 const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 const signin = (req, res = response) => {
   res.send('Login');
 };
 
 const signup = async (req, res = response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors);
+  }
+
   const { name, lastName, email, password } = req.body;
   const user = { name, lastName, email, password };
   const newUser = new Usuario(user);
 
-  // TODO: verificar que el correo no exista
+  // Verificar que el correo no exista
+  const isEmail = await Usuario.findOne({ email });
+  if (isEmail) {
+    return res.status(400).json({
+      error: 'Email already exist.',
+    });
+  }
 
   // Encriptar pass
   const salt = bcrypt.genSaltSync();
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { signin, signup, logout } = require('../controllers/user');
 
 const router = Router();
 
 router.post('/signin', signin);
-router.post('/signup', signup);
+router.post('/signup', [check('email', 'Email not valid.').isEmail()], signup);
 router.get('/logout', logout);
 
 module.exports = router;
